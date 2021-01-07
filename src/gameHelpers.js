7af27da994ab2601fce5ccbbb4a822232b327d48
Seth Harling <sethharling@gmail.com>
2021-01-06 20:55:01 -0700

    added code for numbering blocks around mines

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -16,13 +16,52 @@ export const getMinePositions = (x, y, numMines) => {
 export const createStage = () => {
 	let minePositions = getMinePositions(STAGE_WIDTH, STAGE_HEIGHT, MINES);
 	let rows = [];
-	for(var i = 0; i < STAGE_WIDTH; i++){
+	for(let i = 0; i < STAGE_WIDTH; i++){
 	  let squares = [];
-	  for(var j = 0; j < STAGE_HEIGHT; j++){
+	  for(let j = 0; j < STAGE_HEIGHT; j++){
 		squares.push(<Cell key={16 * i + j} isMine={minePositions.indexOf(16 * i + j) === -1 ? false : true } />);
 	  }
 	  rows.push(<div>{squares}</div>); // board row
 	}
+
+	for (let i = 0; i < rows.length; i++) {
+		for (let j = 0; j < rows[i].length; j++) {
+			if (rows.indexOf(i) === 0) { // if its the top row
+				if (rows.indexOf(j) === 0) { //if its the top left
+					//first check if current square is a mine, if it is go to the next
+					//check east square
+					//check southeast square
+					//check south square
+					//push a cell onto the square with a value stating if its a mine or not, if not,
+					// push with value representing the number of mines around it.
+				} else if (rows.indexOf(j) === STAGE_WIDTH - 1) { // top right
+					//check west
+					//check southwest
+					//check south
+				} else { // other blocks in top row
+					//check west
+					//check southwest
+					//check south
+					//check southeast
+					//check east
+				}
+
+			} else if (rows.indexOf(i) === STAGE_HEIGHT - 1) { // bottom row
+				if (rows.indexOf(j) === 0) { // bottom left
+
+				} else if (rows.indexOf(j) === STAGE_WIDTH - 1) { // bottom right
+
+				} else { // other blocks in bottom row
+
+				}
+
+			} else { // the rest of the blocks 
+
+			}
+
+		}
+	}
+
 	return rows;
 }
 
