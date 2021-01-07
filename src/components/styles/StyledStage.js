25e32dee96e5e17fc8b94da3091ef43adf604daa
Seth Harling <sethharling@gmail.com>
2021-01-07 14:41:23 -0700

    update

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledStage = styled.div`
-    display: grid;
-    grid-template-columns: repeat(16, 35px);
-    grid-template-rows: repeat(16, 35px);
-    width: 600px;
-    height: 600px;
-    background: black;
+	display: grid;
+	grid-template-rows: repeat(16, 35px);
+	grid-template-columns: repeat(16, 35px);
+	width: 600px;
+	height: 600px;
+	background: black;
 `;
--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,4 +1,4 @@
-import Cell from './components/Cell';
+import Cell from "./components/Cell";
 
 export const STAGE_WIDTH = 16;
 export const STAGE_HEIGHT = 16;
@@ -16,55 +16,60 @@ export const getMinePositions = (x, y, numMines) => {
 export const createStage = () => {
 	let minePositions = getMinePositions(STAGE_WIDTH, STAGE_HEIGHT, MINES);
 	let rows = [];
-	for(let i = 0; i < STAGE_WIDTH; i++){
-	  let squares = [];
-	  for(let j = 0; j < STAGE_HEIGHT; j++){
-		squares.push(<Cell key={16 * i + j} isMine={minePositions.indexOf(16 * i + j) === -1 ? false : true } />);
-	  }
-	  rows.push(<div>{squares}</div>); // board row
+	for (let i = 0; i < STAGE_WIDTH; i++) {
+		let squares = [];
+		for (let j = 0; j < STAGE_HEIGHT; j++) {
+			squares.push(
+				<Cell
+					key={16 * i + j}
+					isMine={minePositions.indexOf(16 * i + j) === -1 ? false : true}
+				/>
+			);
+		}
+		rows.push(<div>{squares}</div>); // board row
 	}
 
 	for (let i = 0; i < rows.length; i++) {
 		for (let j = 0; j < rows[i].length; j++) {
-			if (rows.indexOf(i) === 0) { // if its the top row
-				if (rows.indexOf(j) === 0) { //if its the top left
+			if (rows.indexOf(i) === 0) {
+				// if its the top row
+				if (rows.indexOf(j) === 0) {
+					//if its the top left
 					//first check if current square is a mine, if it is go to the next
-					//check east square
-					//check southeast square
-					//check south square
+					//check east square rows[i][j+1]
+					//check southeast square rows[i+1][j+1]
+					//check south square[]
 					//push a cell onto the square with a value stating if its a mine or not, if not,
 					// push with value representing the number of mines around it.
-				} else if (rows.indexOf(j) === STAGE_WIDTH - 1) { // top right
+				} else if (rows.indexOf(j) === STAGE_WIDTH - 1) {
+					// top right
 					//check west
 					//check southwest
 					//check south
-				} else { // other blocks in top row
+				} else {
+					// other blocks in top row
 					//check west
 					//check southwest
 					//check south
 					//check southeast
 					//check east
 				}
-
-			} else if (rows.indexOf(i) === STAGE_HEIGHT - 1) { // bottom row
-				if (rows.indexOf(j) === 0) { // bottom left
-
-				} else if (rows.indexOf(j) === STAGE_WIDTH - 1) { // bottom right
-
-				} else { // other blocks in bottom row
-
+			} else if (rows.indexOf(i) === STAGE_HEIGHT - 1) {
+				// bottom row
+				if (rows.indexOf(j) === 0) {
+					// bottom left
+				} else if (rows.indexOf(j) === STAGE_WIDTH - 1) {
+					// bottom right
+				} else {
+					// other blocks in bottom row
 				}
-
-			} else { // the rest of the blocks 
-
+			} else {
+				// the rest of the blocks
 			}
-
 		}
 	}
 
 	return rows;
-}
+};
 
-export const checkIfMine = () => {
-
-}
+export const checkIfMine = () => {};
